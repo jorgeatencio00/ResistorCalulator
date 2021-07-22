b7d6e62de41a3260a2de86f03acd373ef26fc44d
jorgeatencio00 <58434353+jorgeatencio00@users.noreply.github.com>
2021-07-22 13:08:39 -0400

    Create dbConnect.php

---
+++ dbConnect.php
@@ -0,0 +1,13 @@
+<?php
+    $dbServerName = "localhost";
+    $dbUsername =  "root";
+    $dbPassword = "";
+    $dbName =  "resistorcalcapp";
+    $conn = mysqli_connect($dbServerName, $dbUsername,$dbPassword, $dbName);        // Connect Database
+    if(!$conn)
+    {
+      echo "Connection erorr!";
+    }
+    else {
+      session_start();
+    }
